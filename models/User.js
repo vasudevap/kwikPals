812e7af5c6e133dd5d767a9157773a3bf3b39896
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-09 12:21:11 -0400

    ADDED model user

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model('user', userSchema);
+
+module.exports = User;
