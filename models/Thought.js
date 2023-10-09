72141a61cceefe5761156ead6624c97367742497
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-09 12:30:48 -0400

    ADDED model thought

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = thoughtSchema;
