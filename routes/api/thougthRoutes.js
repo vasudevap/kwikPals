0df78a19d42ef01b5ed9f78f8f7ac3f862108e7a
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-11 19:51:50 -0400

    ADDED route POST reaction in thoughts routes - working...

--- routes/api/thougthRoutes.js
+++ routes/api/thougthRoutes.js
@@ -5,11 +5,14 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  createReaction,
 } = require('../../controllers/thoughtsController');
 
 // /api/thought -> get ALL thougths, post create SINGLE thought 
 router.route('/').get(getThoughts).post(createThought);
 // /api/thought/id -> get, put (update) single thought 
 router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+// /api/thought/id/reaction/id -> post (create) reaction
+router.route('/:id/reaction/:reactionId').post(createReaction);
 
 module.exports = router;
