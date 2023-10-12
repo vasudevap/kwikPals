c39a187e655b95dfea85acc8738a4d16ae11b30a
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-11 20:04:58 -0400

    ADDED route DELETE to remove reaction from thought - working...

--- routes/api/thougthRoutes.js
+++ routes/api/thougthRoutes.js
@@ -4,15 +4,16 @@ const {
   getSingleThought,
   createThought,
   updateThought,
-  deleteThought,
+  removeThought,
   createReaction,
+  removeReaction,
 } = require('../../controllers/thoughtsController');
 
 // /api/thoughts -> get ALL thougths, post create SINGLE thought 
 router.route('/').get(getThoughts).post(createThought);
 // /api/thoughts/id -> get, put (update) single thought 
-router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
-// /api/thoughts/id/reactions/id -> post (create) reaction
-router.route('/:id/reactions').post(createReaction);
+router.route('/:id').get(getSingleThought).put(updateThought).delete(removeThought);
+// /api/thoughts/id/reactions/id -> post (create), delete (remove) reaction
+router.route('/:id/reactions').post(createReaction).delete(removeReaction);
 
 module.exports = router;
