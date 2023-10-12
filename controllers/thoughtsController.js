8a9bf6fa900e5ccbcf7a5a2573f5fe624d13d48e
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-11 20:24:03 -0400

    FIXED route DELETE to remove reaction from thought - TESTED WORKS

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -98,7 +98,7 @@ module.exports = {
     }
   },
   // Delete one thought
-  async deleteThought(req, res) {
+  async removeThought(req, res) {
     console.log('kwikPals: deleting a new thought');
 
     try {
@@ -156,6 +156,25 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+  // Delete one reaction
+  async removeReaction(req, res) {
+    console.log('kwikPals: removing a reaction');
 
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
 
-};
+      if (!thought) {
+        throw new Error('Could not delete reaction from thought in DB');
+      }
+
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+}
--- routes/api/thougthRoutes.js
+++ routes/api/thougthRoutes.js
@@ -13,7 +13,9 @@ const {
 router.route('/').get(getThoughts).post(createThought);
 // /api/thoughts/id -> get, put (update) single thought 
 router.route('/:id').get(getSingleThought).put(updateThought).delete(removeThought);
-// /api/thoughts/id/reactions/id -> post (create), delete (remove) reaction
-router.route('/:id/reactions').post(createReaction).delete(removeReaction);
+// /api/thoughts/id/reactions/id -> post (create) reaction
+router.route('/:id/reactions').post(createReaction)
+// /api/thoughts/id/reactions/id -> delete (remove) a single reaction
+router.route('/:id/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
